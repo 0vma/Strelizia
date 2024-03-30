baa514efd43bdfbaa0976fc798684f0d2674ee70
vma <106908003+0vma@users.noreply.github.com>
2024-03-30 23:44:59 +0100

    Update States.lua

--- Dependencies/States.lua
+++ Dependencies/States.lua
@@ -1,6 +1,6 @@
 local States = {}
 
-States[16510724413] = { --https://www.roblox.com/games/16510724413/Pet-Catchers-ARCADE
+States["16510724413"] = { --https://www.roblox.com/games/16510724413/Pet-Catchers-ARCADE
     Version = "0.0.0 unreleased",
     UpdateLog = "[+] Placeholder\n[-] Placeholder\n[*] Placeholder",
     Status = {
