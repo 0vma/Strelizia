1fcb69ecacdbc06b10198ee232ef44337bb7049c
vma <106908003+0vma@users.noreply.github.com>
2024-04-01 20:13:39 +0200

    Update States.lua

--- Dependencies/States.lua
+++ Dependencies/States.lua
@@ -1,8 +1,8 @@
 local States = {}
 
 States[16510724413] = { --https://www.roblox.com/games/16510724413/Pet-Catchers-ARCADE
-    Version = "0.0.0 unreleased",
-    UpdateLog = "[+] Placeholder\n[-] Placeholder\n[*] Placeholder",
+    Version = "1.0.2 BETA",
+    UpdateLog = "> Improved Features </n[*] Auto Merchant Fixed\n[*] Auto Codes Fixed\n[*] Fixed Boss & Minigame Labels\n> Possible Issues <\n[?] - Randomly being teleported inside of the boss area while both fighting the boss and auto fishing",
     Status = {
         Downtime = false,
         DowntimeReason = "..."
