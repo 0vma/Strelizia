31f231fed2cb50831447b991044bc0979ccf8370
vma <106908003+0vma@users.noreply.github.com>
2024-04-04 09:33:49 +0200

    Update and rename Codes to Codes.lua

---
+++ Games/16510724413/Modules/Codes.lua
@@ -0,0 +1,219 @@
+{
+    ["runes"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Seafarer Rune",
+                ["Amount"] = 1
+            },
+            [2] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Fatal Strike Rune",
+                ["Amount"] = 1
+            },
+            [3] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Critical Hit Rune",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["lucky"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Lucky Elixir",
+                ["Amount"] = 3
+            }
+        }
+    },
+    ["superboost"] = {
+        ["EndsAt"] = 1711843200000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Golden Cherry",
+                ["Amount"] = 3
+            },
+            [2] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Super Lucky Elixir",
+                ["Amount"] = 3
+            },
+            [3] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Prismatic Sundae",
+                ["Amount"] = 1
+            },
+            [4] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Egg Elixir",
+                ["Amount"] = 5
+            }
+        }
+    },
+    ["cherry"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Golden Cherry",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["easter"] = {
+        ["EndsAt"] = 1712275200000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Elite Mystery Egg",
+                ["Amount"] = 2000
+            },
+            [2] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Egg Elixir",
+                ["Amount"] = 3
+            }
+        }
+    },
+    ["cherries"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Golden Cherry",
+                ["Amount"] = 2
+            }
+        }
+    },
+    ["gravypet"] = {
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Pet",
+                ["Name"] = "Gravycatman Pal",
+                ["Amount"] = 1
+            },
+            [2] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Wildberry",
+                ["Amount"] = 200
+            },
+            [3] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Coin Elixir",
+                ["Amount"] = 3
+            }
+        }
+    },
+    ["update1"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Golden Cherry",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["looksmaxx"] = {
+        ["EndsAt"] = 1712059200000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Pet",
+                ["Name"] = "Moggy",
+                ["Amount"] = 1
+            },
+            [2] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Egg Elixir",
+                ["Amount"] = 3
+            },
+            [3] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Golden Cherry",
+                ["Amount"] = 3
+            }
+        }
+    },
+    ["russoplays"] = {
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Pet",
+                ["Name"] = "Russo Pal",
+                ["Amount"] = 1
+            },
+            [2] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Wildberry",
+                ["Amount"] = 200
+            },
+            [3] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Coin Elixir",
+                ["Amount"] = 3
+            }
+        }
+    },
+    ["release"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Currency",
+                ["Currency"] = "Coins",
+                ["Amount"] = 500
+            },
+            [2] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Coin Elixir",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["void"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Void Cherry",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["ilovefishing"] = {
+        ["EndsAt"] = 1710374400000,
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Sea Elixir",
+                ["Amount"] = 3
+            },
+            [2] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Seafarer Rune",
+                ["Amount"] = 1
+            }
+        }
+    },
+    ["brite"] = {
+        ["Items"] = {
+            [1] = {
+                ["Type"] = "Pet",
+                ["Name"] = "Brite Pal",
+                ["Amount"] = 1
+            },
+            [2] = {
+                ["Type"] = "Treat",
+                ["Name"] = "Wildberry",
+                ["Amount"] = 200
+            },
+            [3] = {
+                ["Type"] = "Powerup",
+                ["Name"] = "Coin Elixir",
+                ["Amount"] = 3
+            }
+        }
+    }
+}
