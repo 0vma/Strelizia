bf849ecd323f269b4362667c4c5788dc200f84e5
vma <106908003+0vma@users.noreply.github.com>
2024-04-14 19:15:48 +0200

    Update States.lua

--- Dependencies/States.lua
+++ Dependencies/States.lua
@@ -1,8 +1,8 @@
 local States = {}
 
 States[16510724413] = { --https://www.roblox.com/games/16510724413/Pet-Catchers-ARCADE
-    Version = "1.0.5 BETA",
-    UpdateLog = "> Added Features <\n[+] Auto Prismatic Tomes\n> Improved Features <\n[*] Auto Merchant Improved\n[*] Auto Codes Fixed\n[*] Fixed Boss Teleports\n[*] Improved Performance\n",
+    Version = "1.1.3 BETA",
+    UpdateLog = "discord.gg/strelizia",
     Status = {
         Downtime = false,
         DowntimeReason = "..."
