c6cb52ab7348ad079e13171e6fffb7d296dfbb07
vma <106908003+0vma@users.noreply.github.com>
2024-11-12 22:30:26 +0100

    Create Emojis.lua

---
+++ Games/18901165922/Modules/Emojis.lua
@@ -0,0 +1,26 @@
+return {
+    ['gem'] = "<:gems:1305298012871852072>",
+    ['hourglass'] = "<:clock:1305639684713615440>",
+    ['dice'] = "<:dice:1305291879998558339>",
+    ['person'] = "<:person:1305298114265219094>",
+    ['letter_x'] = "<:letter_x:1305284794044776622>",
+    ['pet'] = "<:pets:1305298014868344883>",
+    ['rarity'] = "<:rarity:1305298201779372190>",
+    ['serial'] = "<:serial:1305298247476052018>",
+    ['blazing_dice'] = "<:blazing_dice:1305641913214435429>",
+    ['golden_dice'] = "<:golden_dice:1305641910681075812>",
+    ['upgrades'] = "<:upgrades:1305641908420476928>",
+
+    -- // currency 
+    ['currency_Coins'] = "<:coin:1305641916901232681>",
+    ['currency_Diamonds'] = "<:gems:1305298012871852072>",
+    ['currency_Halloween'] = "<:Candy:1305647828218937394>",
+
+    -- // misc items
+    ['misc_Crystal Key'] = "<:CrystalKey:1305644902776049704>",
+    ['misc_Crystal Key Upper Half'] = "<:CrystalKeyUpperHalf:1305644900762517514>",
+    ['misc_Crystal Key Lower Half'] = "<:CrystalKeyLowerHalf:1305644898925416458>",
+    ['misc_Secret Key'] = "<:SecretKey:1305644897298153572>",
+    ['misc_Index Token'] = "<:IndexToken:1305644904805957633>",
+    ['misc_Fishing Token'] = "<:fishing_coin:1305641914657411164>",
+}
