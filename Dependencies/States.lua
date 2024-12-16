4f1871d64ab0fddeee27f8de8c7f8404b96cf21c
Pryxo <142629201+Pryxo@users.noreply.github.com>
2024-12-16 17:33:46 +0100

    Update States.lua

--- Dependencies/States.lua
+++ Dependencies/States.lua
@@ -39,7 +39,7 @@ States.data = {
                 ['Emojis'] = "https://raw.githubusercontent.com/0vma/Strelizia/refs/heads/main/Games/18901165922/Modules/Emojis.lua"
             },
             Version = "0.0.1 BETA",
-            LastUpdated = 1731534966,
+            LastUpdated = 1734366816,
             Script = "https://api.luarmor.net/files/v3/loaders/467d3f58831c5b64fe4c8e8491534846.lua",
             SupportedVersion = 536.25,
             UpdateLog = '<font color="rgb(255, 255, 0)">[/] Fixed Bugs</font>',
