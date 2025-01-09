5661b9f3b513a8fb039abc79d2eac2489c3aff86
Pryxo <142629201+Pryxo@users.noreply.github.com>
2025-01-10 00:11:19 +0100

    Added Bot Lobby

--- Dependencies/States.lua
+++ Dependencies/States.lua
@@ -18,7 +18,21 @@ States.data = {
             SupportedVersion = 536.25,
             UpdateLog = '<stroke color="#000000" thickness="2"><font color="rgb(0, 255, 0)">[+] Added new obstacle course levels for extra challenges</font>\n<font color="rgb(0, 255, 0)">[+] Introduced a new character customization feature</font>\n<font color="rgb(0, 255, 0)">[+] Added in-game achievements to track progress</font>\n<font color="rgb(255, 0, 0)">[-] Removed outdated in-game tutorial to make space for new one</font>\n<font color="rgb(255, 0, 0)">[-] Removed old Halloween-themed items</font>\n<font color="rgb(255, 255, 0)">[*] Fixed bug causing players to fall through the floor</font>\n<font color="rgb(255, 255, 0)">[*] Fixed issue with level progress not saving correctly</font>\n<font color="rgb(255, 255, 0)">[*] Fixed UI overlapping on smaller screen devices</font>\n<font color="rgb(255, 255, 0)">[*] Improved performance for smoother gameplay experience</font></stroke>',
         },
-        ["18901165922"] = {
+        ["18901165922"] = { -- Pets Go
+            Shutdown = {
+                Status = false,
+                Reason = "Shutdown reason here"
+            },
+            Modules = {
+                ['Emojis'] = "https://raw.githubusercontent.com/0vma/Strelizia/refs/heads/main/Games/18901165922/Modules/Emojis.lua"
+            },
+            Version = "0.0.2 BETA",
+            LastUpdated = 1731534966,
+            Script = "https://api.luarmor.net/files/v3/loaders/df91992d2ad7b8dd3432b116e3ff2a31.lua",
+            SupportedVersion = 536.25,
+            UpdateLog = '<font color="rgb(255, 255, 0)">[/] Fixed Bugs</font>',
+        },
+	["125597998995485"] = { -- Pets Go Bot Lobby
             Shutdown = {
                 Status = false,
                 Reason = "Shutdown reason here"
